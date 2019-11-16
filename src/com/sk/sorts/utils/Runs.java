838e802b97380349fecd2054d06b8eade53dfb03
Samra Kasim <samra@blackcape.io>
2019-11-16 15:26:30 -0500

    basic quick sort working

--- src/com/sk/sorts/utils/Runs.java
+++ src/com/sk/sorts/utils/Runs.java
@@ -12,8 +12,8 @@ public class Runs {
   public static void processSorts(File runFile, int numberOfRuns, File inProcessFile, String outputDir, Scanner scanner) {
     FileName fileName = parseFileName(inProcessFile.getName());
     int arraySize = fileName.getFileSize();
-    System.out.println("PROCESSING: " + fileName);
-    writeFileLineByLine(runFile, "PROCESSING: " + fileName.toString());
+    System.out.println("Now processing: " + fileName.toString());
+    writeFileLineByLine(runFile, "Now processing: " + fileName.toString());
 
     int[] origArray = new int[arraySize];
     int elemCount = 0;
@@ -26,26 +26,26 @@ public class Runs {
     }
 
 
-    System.out.println(Arrays.toString(origArray));
     File sortedOutputFile = new File(outputDir + "out_" + fileName.getOriginalFileName());
 
     if (arraySize == 50) {
-      writeFileLineByLine(sortedOutputFile, "INPUT: "+ fileName.toString());
+      writeFileLineByLine(sortedOutputFile, "Input: "+ fileName.toString());
       writeArray(sortedOutputFile, origArray, arraySize);
+      System.out.println("Input: " + fileName.toString());
+      System.out.println(Arrays.toString(origArray));
     }
 
     // Quicksort with first item as pivot and partitions of size one and two as stopping cases
     basicQuickSortRuns(runFile, sortedOutputFile, numberOfRuns, origArray, arraySize);
-
 
     // Quicksort with first item as pivot, 100 as stopping case and insert sort to finish
 
   }
 
 
-  public static void basicQuickSortRuns(File runFile, File sortedOutputFile, int numberOfRuns, int[] origArray, int arraySize) {
-    System.out.println("\nALGORITHM TYPE: Quicksort with first item as pivot and partitions of size one and two as stopping cases. \n");
-    writeFileLineByLine(sortedOutputFile, "\nALGORITHM TYPE: Quicksort with first item as pivot and partitions of size one and two as stopping cases. \n");
+  private static void basicQuickSortRuns(File runFile, File sortedOutputFile, int numberOfRuns, int[] origArray, int arraySize) {
+    System.out.println("Algorithm type applied: Quicksort with first item as pivot and partitions of size one and two as stopping cases.");
+    writeFileLineByLine(sortedOutputFile, "Algorithm type applied: Quicksort with first item as pivot and partitions of size one and two as stopping cases.");
 
     int runCount = 0;
     long beginBasicQuickSort = System.nanoTime();
@@ -56,9 +56,9 @@ public class Runs {
       if (!isPrinted) {
         // If array size is 50 then print the sorted array to an output file
         if (arraySize == 50 && !isPrinted) {
-          writeFileLineByLine(sortedOutputFile, "SORTED OUTPUT: ");
+          writeFileLineByLine(sortedOutputFile, "Sorted output: ");
           writeArray(sortedOutputFile, arrayCopy, arraySize);
-          System.out.println("SORTED OUTPUT:" + Arrays.toString(arrayCopy));
+          System.out.println("Sorted output:" + Arrays.toString(arrayCopy));
           isPrinted = true;
         }
       }
@@ -67,11 +67,13 @@ public class Runs {
     long endBasicQuickSort = System.nanoTime();
     long totalTimeBasicQuickSort = (endBasicQuickSort - beginBasicQuickSort)/numberOfRuns;
 
-
-
-    System.out.println("TOTAL BASIC SORT RUN TIME: " + totalTimeBasicQuickSort);
-    writeFileLineByLine(runFile, " Run time of quicksort with first item as pivot and partitions of size one and two as stopping cases is [" + totalTimeBasicQuickSort + "]. \n");
+    System.out.println("Total sort run time: " + totalTimeBasicQuickSort +"\n");
+    writeFileLineByLine(sortedOutputFile, "Total sort run time: " + totalTimeBasicQuickSort + "\n");
+    writeFileLineByLine(runFile, "  Run time of quicksort with first item as pivot and partitions of size one and two as stopping cases is [" + totalTimeBasicQuickSort + "]. \n");
   }
 
+  private static void optimizedQuickSortRuns(File runFile, File sortedOutputFile, int numberOfRuns, int[] origArray, int arraySize, int stoppingCase) {
+
+  }
 
 }
